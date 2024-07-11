c14ff08d070f1e0dd7edf305be1ab7a7a6a16008
Neil <neilalfred93@gmail.com>
2024-07-11 11:12:47 +0800

    reset the code format

--- src/mr/worker.go
+++ src/mr/worker.go
@@ -10,8 +10,8 @@ import "hash/fnv"
 // Map functions return a slice of KeyValue.
 //
 type KeyValue struct {
- Key   string
- Value string
+	Key   string
+	Value string
 }
 
 //
@@ -19,9 +19,9 @@ type KeyValue struct {
 // task number for each KeyValue emitted by Map.
 //
 func ihash(key string) int {
- h := fnv.New32a()
- h.Write([]byte(key))
- return int(h.Sum32() & 0x7fffffff)
+	h := fnv.New32a()
+	h.Write([]byte(key))
+	return int(h.Sum32() & 0x7fffffff)
 }
 
 
@@ -29,27 +29,12 @@ func ihash(key string) int {
 // main/mrworker.go calls this function.
 //
 func Worker(mapf func(string, string) []KeyValue,
- reducef func(string, []string) string) {
+	reducef func(string, []string) string) {
 
- // Your worker implementation here.
- for {
-  response := doHeartbeat()
-  log.Printf("Worker: receive coordinator's heartbeat %v \n", response)
-  switch responese.JobType{
-  case MapJob:
-   doMapTask(mapf, response)
-  case ReduceJob:
-   doReduceTask(reducef, response)
-  case WaitJob:
-   time.Sleep(1 * time.Second)
-  case CompleteJob:
-   return
-  default:
-   panic(fmt.Sprintf("unexpected jobType %v", response.JobType))
-  }
- }
- // uncomment to send the Example RPC to the coordinator.
- // CallExample()
+	// Your worker implementation here.
+
+	// uncomment to send the Example RPC to the coordinator.
+	// CallExample()
 
 }
 
@@ -60,26 +45,26 @@ func Worker(mapf func(string, string) []KeyValue,
 //
 func CallExample() {
 
- // declare an argument structure.
- args := ExampleArgs{}
+	// declare an argument structure.
+	args := ExampleArgs{}
 
- // fill in the argument(s).
- args.X = 99
+	// fill in the argument(s).
+	args.X = 99
 
- // declare a reply structure.
- reply := ExampleReply{}
+	// declare a reply structure.
+	reply := ExampleReply{}
 
- // send the RPC request, wait for the reply.
- // the "Coordinator.Example" tells the
- // receiving server that we'd like to call
- // the Example() method of struct Coordinator.
- ok := call("Coordinator.Example", &args, &reply)
- if ok {
-  // reply.Y should be 100.
-  fmt.Printf("reply.Y %v\n", reply.Y)
- } else {
-  fmt.Printf("call failed!\n")
- }
+	// send the RPC request, wait for the reply.
+	// the "Coordinator.Example" tells the
+	// receiving server that we'd like to call
+	// the Example() method of struct Coordinator.
+	ok := call("Coordinator.Example", &args, &reply)
+	if ok {
+		// reply.Y should be 100.
+		fmt.Printf("reply.Y %v\n", reply.Y)
+	} else {
+		fmt.Printf("call failed!\n")
+	}
 }
 
 //
@@ -88,19 +73,19 @@ func CallExample() {
 // returns false if something goes wrong.
 //
 func call(rpcname string, args interface{}, reply interface{}) bool {
- // c, err := rpc.DialHTTP("tcp", "127.0.0.1"+":1234")
- sockname := coordinatorSock()
- c, err := rpc.DialHTTP("unix", sockname)
- if err != nil {
-  log.Fatal("dialing:", err)
- }
- defer c.Close()
+	// c, err := rpc.DialHTTP("tcp", "127.0.0.1"+":1234")
+	sockname := coordinatorSock()
+	c, err := rpc.DialHTTP("unix", sockname)
+	if err != nil {
+		log.Fatal("dialing:", err)
+	}
+	defer c.Close()
 
- err = c.Call(rpcname, args, reply)
- if err == nil {
-  return true
- }
+	err = c.Call(rpcname, args, reply)
+	if err == nil {
+		return true
+	}
 
- fmt.Println(err)
- return false
+	fmt.Println(err)
+	return false
 }
